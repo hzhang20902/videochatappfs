0cf233f1b81fd5135212e2d51ae7b79e15859d69
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-09-23 19:16:00 -0400

    client logic set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Typography, AppBar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import VideoPlayer from './components/VideoPlayer'
+import Options from './components/Options'
+import Notifications from './components/Notifications'
+import { FullscreenExit } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid black',
+
+        [theme.breakpoints.down('xs')]: {
+            width: '90%',
+        }
+    },
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+    },
+ }));
 
 const App = () => {
+    const classes = useStyles();
   return (
-    <div>
-        Video Chat App
+    <div className={classes.wrapper}>
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+            <Typography variant='h2' align='center'>Vid Chat</Typography>
+        </AppBar>
+        <VideoPlayer />
+        <Options>
+            <Notifications />
+        </Options>
+        
     </div>
   )
 }
